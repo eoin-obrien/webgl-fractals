52e621f0c8604c57173c33816066ac8e122e5db3
Eoin O'Brien <eoinobrien910@gmail.com>
2022-08-31 03:22:57 +0100

    Implement supersampling

--- pages/mandelbrot.tsx
+++ pages/mandelbrot.tsx
@@ -1,114 +1,10 @@
-import { MapControls, OrbitControls, Plane, ScreenQuad, shaderMaterial } from '@react-three/drei'
-import { Canvas, extend, useFrame, useThree } from '@react-three/fiber'
-import { useDrag, useGesture } from '@use-gesture/react'
+import { Canvas } from '@react-three/fiber'
+import { useGesture } from '@use-gesture/react'
 import { useControls } from 'leva'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { FC, useRef, useState } from 'react'
-import { Vec2, Vector2, Vector2Tuple } from 'three'
-import styles from '../styles/Home.module.css'
-
-const Mandelbrot = shaderMaterial(
-  { time: 0, resolution: new Vector2(), maxIterations: 100, gamma: 1, center: new Vector2(), zoom: 1 },
-    /*glsl*/`
-      void main() {
-        gl_Position = vec4(position, 1.0);
-      }
-      `,
-    /*glsl*/`
-      uniform float time;
-      uniform vec2 resolution;
-      uniform vec2 center;
-      uniform float zoom;
-      uniform float gamma;
-      uniform int maxIterations;
-
-      // cosine based palette, 4 vec3 params
-      vec3 palette( in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d )
-      {
-        return a + b*cos( 6.28318*(c*t+d) );
-      }
-
-      float mandelbrot(vec2 c) {
-        const float B = 256.0;
-
-        float n = 0.0;
-        vec2 z  = vec2(0.0);
-        for( int i=0; i<maxIterations; i++ )
-        {
-            z = vec2( z.x*z.x - z.y*z.y, 2.0*z.x*z.y ) + c; // z = z² + c
-            if( dot(z,z)>(B*B) ) {
-              return n - log2(log2(dot(z,z))) + 4.0;
-            };
-            n += 1.0;
-        }
-
-        return float(maxIterations);
-    }
-
-      void main() {
-        float scale = 1.0 / zoom / min(resolution.x, resolution.y);
-        vec2 p = center + (gl_FragCoord.xy - 0.5 * resolution) * scale;
-
-        float i = mandelbrot(p);
-
-        if (i < float(maxIterations)) {
-          float normalized = pow(i, gamma);
-          // gl_FragColor = vec4(vec3(pow(normalized, gamma)), 1.0);
-          gl_FragColor = vec4(palette(normalized, vec3(0.5, 0.5, 0.5), vec3(0.5, 0.5, 0.5), vec3(1.0, 1.0, 1.0),	vec3(0.00, 0.33, 0.67)), 1.0);
-          gl_FragColor = vec4(palette(normalized, vec3(0.5, 0.5, 0.5), vec3(0.5, 0.5, 0.5), vec3(1.0, 1.0, 1.0),	vec3(0.20, 0.10, 0.00)), 1.0);
-        } else {
-          gl_FragColor = vec4(vec3(0.0), 1.0);
-        }
-      }
-      `
-)
-
-extend({ Mandelbrot })
-
-type MandelbrotImpl = {
-  resolution: number[]
-  center?: number[]
-  zoom?: number
-  gamma?: number
-  maxIterations?: number
-} & JSX.IntrinsicElements['shaderMaterial']
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      mandelbrot: MandelbrotImpl
-    }
-  }
-}
-
-interface MandelbrotSceneProps {
-  center: [x: number, y: number]
-  zoom: number
-  gamma: number
-  maxIterations: number
-}
-
-const MandelbrotScene: FC<MandelbrotSceneProps> = (props) => {
-  const size = useThree((state) => state.size);
-  const dpr = useThree((state) => state.viewport.dpr);
-  const ref = useRef<MandelbrotImpl>(null!)
-  useFrame((state) => {
-    if (ref.current.uniforms) {
-      ref.current.uniforms.time.value = state.clock.elapsedTime
-    }
-  })
-
-  return (
-    <ScreenQuad>
-      <mandelbrot
-        ref={ref as any}
-        {...props}
-        resolution={[size.width * dpr, size.height * dpr]} />
-    </ScreenQuad>
-  );
-}
+import { useRef, useState } from 'react'
+import { Vector2Tuple } from 'three'
+import { MandelbrotScene } from '../shaders/mandelbrot'
 
 const zoomFactor = 1.1;
 
@@ -153,6 +49,7 @@ const MandelbrotPage: NextPage = () => {
   const controls = useControls({
     gamma: 0.2,
     maxIterations: 100,
+    supersample: { value: 2, min: 1, max: 16, step: 1 },
   })
   return (
     <Canvas ref={canvasRef} linear flat frameloop='demand' style={{ touchAction: 'none' }} {...bind()}>
---
+++ shaders/mandelbrot.tsx
@@ -0,0 +1,124 @@
+import { ScreenQuad, shaderMaterial } from '@react-three/drei'
+import { extend, useFrame, useThree } from '@react-three/fiber'
+import { FC, useRef } from 'react'
+import { Vector2, Vector2Tuple } from 'three'
+
+const Mandelbrot = shaderMaterial(
+  {
+    time: 0,
+    resolution: new Vector2(),
+    maxIterations: 100,
+    supersample: 1,
+    gamma: 1,
+    center: new Vector2(),
+    zoom: 1
+  },
+  /*glsl*/`
+    void main() {
+      gl_Position = vec4(position, 1.0);
+    }
+    `,
+  /*glsl*/`
+    uniform float time;
+    uniform vec2 resolution;
+    uniform vec2 center;
+    uniform float zoom;
+    uniform float gamma;
+    uniform int maxIterations;
+    uniform int supersample;
+
+    // cosine based palette, 4 vec3 params
+    vec3 palette( in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d )
+    {
+      return a + b*cos( 6.28318*(c*t+d) );
+    }
+
+    float mandelbrot(vec2 c) {
+      const float B = 256.0;
+
+      float n = 0.0;
+      vec2 z  = vec2(0.0);
+      for( int i=0; i<maxIterations; i++ )
+      {
+          z = vec2( z.x*z.x - z.y*z.y, 2.0*z.x*z.y ) + c; // z = z² + c
+          if( dot(z,z)>(B*B) ) {
+            return n - log2(log2(dot(z,z))) + 4.0;
+          };
+          n += 1.0;
+      }
+
+      return float(maxIterations);
+    }
+
+    vec3 toColor(float k) {
+      if (k < float(maxIterations)) {
+        return palette(pow(k, gamma), vec3(0.5, 0.5, 0.5), vec3(0.5, 0.5, 0.5), vec3(1.0, 1.0, 1.0),	vec3(0.20, 0.10, 0.00));
+      } else {
+        return vec3(0.0);
+      }
+    }
+
+    void main() {
+      float scale = 1.0 / zoom / min(resolution.x, resolution.y);
+      vec2 p = center + (gl_FragCoord.xy - 0.5 * resolution) * scale;
+
+      float subpixel = scale / float(supersample);
+      vec3 color = vec3(0.0);
+      vec2 origin = p - 0.5 * scale;
+      for (int i = 0; i < supersample; i++) {
+        for (int j = 0; j < supersample; j++) {
+          float k = mandelbrot(vec2(
+            origin.x + (float(i) + 0.5) * subpixel,
+            origin.y + (float(j) + 0.5) * subpixel
+          ));
+          color += toColor(k);
+        }
+      }
+
+      color /= pow(float(supersample), 2.0);
+
+      gl_FragColor = vec4(color, 1.0);
+    }
+    `
+)
+
+extend({ Mandelbrot })
+
+type MandelbrotImpl = {
+  resolution: Vector2Tuple
+  center?: Vector2Tuple
+  zoom?: number
+  gamma?: number
+  maxIterations?: number
+  supersample?: number
+} & JSX.IntrinsicElements['shaderMaterial']
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      mandelbrot: MandelbrotImpl
+    }
+  }
+}
+
+interface MandelbrotSceneProps extends Partial<MandelbrotImpl> {}
+
+export const MandelbrotScene: FC<MandelbrotSceneProps> = (props) => {
+  const size = useThree((state) => state.size);
+  const dpr = useThree((state) => state.viewport.dpr);
+  const ref = useRef<MandelbrotImpl>(null!)
+  useFrame((state) => {
+    if (ref.current.uniforms) {
+      ref.current.uniforms.time.value = state.clock.elapsedTime
+    }
+  })
+
+  return (
+    <ScreenQuad>
+      <mandelbrot
+        ref={ref as any}
+        {...props}
+        resolution={[size.width * dpr, size.height * dpr]} />
+    </ScreenQuad>
+  );
+}
